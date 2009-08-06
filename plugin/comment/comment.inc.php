54bde7d566176236fbfcb5bee319625f06c749fd
kaz6120 <kaz6120@gmail.com>
2009-08-06 17:24:27 +0900

    updated "comment" plugin.

--- plugin/comment/comment.inc.php
+++ plugin/comment/comment.inc.php
@@ -12,7 +12,7 @@ class Plugin_comment extends Plugin
 		static $count = array();
 		
 		
-		if(mb_ereg('^(.*?),(.*)$', $param1, $m)){
+		if (mb_ereg('^(.*?),(.*)$', $param1, $m)) {
 			$type = strtolower(trim($m[1])) == 'below' ? 'below' : 'above';
 			$input = strtolower(trim($m[2])) == 'textarea' ? 'textarea' : 'line';
 		} else{
@@ -21,7 +21,7 @@ class Plugin_comment extends Plugin
 		}
 		
 		$smarty = $this->getsmarty();
-		if(!isset($count[$page->getpagename()])){
+		if (!isset($count[$page->getpagename()])) {
 			$count[$page->getpagename()] = 0;
 		}
 		$smarty->assign('num', $count[$page->getpagename()]++);
@@ -38,24 +38,24 @@ class Plugin_comment extends Plugin
 	
 	function do_url()
 	{
-		if(!keys_exists(Vars::$post, 'num', 'type', 'pagename')){
+		if (!keys_exists(Vars::$post, 'num', 'type', 'pagename')) {
 			throw new PluginException('パラメータが足りません。', $this);
 		}
-		if(trim(Vars::$post['text']) == ''){
+		if (trim(Vars::$post['text']) == '') {
 			redirect(Page::getinstance(Vars::$post['pagename']));
 		}
 		
 		$page = Page::getinstance(Vars::$post['pagename']);
 		$source = explode("\n", Page::getinstance(Vars::$post['pagename'])->getsource());
 		$count = 0;
-		for($i = 0; $i < count($source); $i++){
-			if(mb_ereg('^#comment(?:\s|\(|{|$)', $source[$i])){
-				if($count == Vars::$post['num']){
+		for($i = 0; $i < count($source); $i++) {
+			if (mb_ereg('^#comment(?:\s|\(|{|$)', $source[$i])) {
+				if ($count == Vars::$post['num']) {
 					$name = trim(Vars::$post['name']);
 					$text = mb_ereg_replace('{', '&173;', Vars::$post['text']);
 					$text = mb_ereg_replace('}', '&175;', $text);
 					$time = date('Y-m-d H:i:s', time());
-					$str = ":[[{$name}>UserPage/{$name}]] &size(80%){{$time}}:";
+					$str = ":[[{$name}>UserPage/{$name}]] &size(80%) {{$time}}:";
 					$str .= mb_ereg("\n", $str) ? "\n#block{{$text}}" : " $text";
 					$n = $i + (Vars::$post['type'] == 'below' ? 1 : 0);
 					array_splice($source, $n, 0, $str);
