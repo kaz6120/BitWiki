2b821dcb51ebcc3529d7b3abab0eacd2d05cefd7
kaz6120 <kaz6120@gmail.com>
2009-08-09 11:37:15 +0900

    updated version number

--- hideable/version.inc.php
+++ hideable/version.inc.php
@@ -2,8 +2,8 @@
 /*
  * $Id: version.inc.php,v 1.4 2005/09/05 10:27:36 youka Exp $
  *
- * @version: 9.8.6
+ * @version: 9.8.9
  */
 
 define('KINOWIKI_VERSION', '2.1');
-define('KINOWIKI_REV_VERSION', '9.8.6');
+define('KINOWIKI_REV_VERSION', '9.8.9');
